bf6de461e850d13d0de05b5ea453e6a2666195ad
Nicolas Limare <nicolas@limare.net>
2015-11-04 02:06:46 +0900

    no more post-install step, will do that with chef

--- update.sh
+++ update.sh
@@ -3,4 +3,3 @@
 RAWGHMASTER=raw.githubusercontent.com/nilx/RASP/master
 
 wget https://$RAWGHMASTER/boot/installer-config.txt -O installer-config.txt
-wget https://$RAWGHMASTER/boot/post-install.txt -O post-install.txt
