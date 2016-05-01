c6f977a808b90912054e186af6ac1afd6befbd9f
Nicolas Limare <nicolas@limare.net>
2016-05-01 17:33:36 +0900

    no more sudo, no more password for `user`

--- chef/cookbooks/rasp/recipes/user.rb
+++ chef/cookbooks/rasp/recipes/user.rb
@@ -3,15 +3,8 @@
 package 'ruby-shadow'
 
 user 'user' do
-  password 'user' # can't use an empty password, not accepted by sudo
+  password ''
   shell '/bin/bash'
   home '/home/user'
   manage_home true
-end
-
-package 'sudo'
-
-group 'sudo' do
-  append true
-  members 'user'
 end
