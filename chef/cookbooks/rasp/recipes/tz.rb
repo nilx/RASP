9cf0698a61c453d7c60d34c6fb8c69d731cc8e80
Nicolas Limare <nicolas@limare.net>
2016-05-01 16:05:21 +0900

    get timezone via Ruby http calls

--- chef/cookbooks/rasp/recipes/tz.rb
+++ chef/cookbooks/rasp/recipes/tz.rb
@@ -1,9 +1,13 @@
 # set timezone
 
-# TODO: use Ruby
-ip = `curl -s icanhazip.com`
-tz = "curl -s geoip.cf/api/#{ip}/timezone"
+require 'open-uri'
+open('http://icanhazip.com/') do |f|
+  ip = f.read.strip
+end
+open("http://geoip.cf/api/#{ip}/timezone") do |f|
+  tz = f.read.strip
+end
 
 file "/etc/timezone" do
-  content "#{tz}"
+  content "#{tz}\n"
 end
